867a78cb496b70bbcff4f604c2f7636c4b845c30
giospes <giovannijacopo.spes@gmail.com>
2023-05-16 13:54:42 +0200

    done

---
+++ Movie.php
@@ -0,0 +1,48 @@
+<?php
+
+    class Movie{
+        private string $title;
+        private string $director;
+        private $genre;
+        private int $releaseYear;
+
+        // Constructor
+        public function __construct($title, $director, $genre, $releaseYear) {
+            $this->title = $title;
+            $this->director = $director;
+            $this->genre = $genre;
+            $this->releaseYear = $releaseYear;
+        }
+
+        public function getTitle() {
+            return $this->title;
+        }
+
+        public function setTitle($title) {
+            $this->title = $title;
+        }
+
+        public function getDirector() {
+            return $this->director;
+        }
+
+        public function setDirector($director) {
+            $this->director = $director;
+        }
+
+        public function getGenre() {
+            return $this->genre;
+        }
+
+        public function setGenre($genre) {
+            $this->genre = $genre;
+        }
+
+        public function getReleaseYear() {
+            return $this->releaseYear;
+        }
+
+        public function setReleaseYear($releaseYear) {
+            $this->releaseYear = $releaseYear;
+        }
+    }
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include  __DIR__. '/Movie.php' ;
+    $movie1 = new Movie("The Shawshank Redemption", "Frank Darabont", "Drama", 1994);
+    $movie2 = new Movie("The Wolf of Wall Street", "Martin Scorsese", "Drama", 2013);
+    var_dump($movie2)
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
